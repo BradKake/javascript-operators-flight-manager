2a340841bc35538be7ab135cd902cc090f2f089e
BradKake <95509523+BradKake@users.noreply.github.com>
2022-08-01 20:28:14 +1000

    completed module

--- logic/flights.js
+++ logic/flights.js
@@ -1,13 +1,22 @@
 function Flights() {
-    return function calculateNumberOfFlights(numPassengers, capacity) {
-        if (numPassengers % capacity == 0) {
-            let numFlights = numPassengers / capacity;
-        } else {
-            numFlights = numPassengers / capacity + 1;
-            return {calculateNumberOfFlights};
+    function calculateNumberOfFlights(numPassengers, capacity) {
+        let numFlights;
+
+        if (numPassengers < 0) {
+            throw Error ("The number of passengers must be a positive integer value");
         }
+        if (capacity < 0) {
+            throw Error ("The capacity of the flight must be a positive integer value");
+        }
+
+        if (numPassengers % capacity === 0) {
+            numFlights = numPassengers / capacity;
+        } else {
+            numFlights = Math.floor(numPassengers/capacity) + 1;
+        }
+        return numFlights;
     }
+    return {calculateNumberOfFlights};
 }
 
 module.exports = Flights();
-
