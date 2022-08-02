f51eb4ab0c3709513cf607189ae6a891803a10b3
BradKake <95509523+BradKake@users.noreply.github.com>
2022-08-02 19:23:15 +1000

    completed module

--- logic/flights.js
+++ logic/flights.js
@@ -1,22 +1,48 @@
 function Flights() {
+
     function calculateNumberOfFlights(numPassengers, capacity) {
         let numFlights;
 
         if (numPassengers < 0) {
-            throw Error ("The number of passengers must be a positive integer value");
+            throw new Error("The number of passengers must be a positive integer value");
         }
         if (capacity < 0) {
-            throw Error ("The capacity of the flight must be a positive integer value");
+            throw new Error("The capacity of the flight must be a positive integer value");
         }
 
-        if (numPassengers % capacity === 0) {
+        if (numPassengers % capacity == 0) {
             numFlights = numPassengers / capacity;
         } else {
-            numFlights = Math.floor(numPassengers/capacity) + 1;
+            numFlights = Math.floor(numPassengers / capacity) + 1;
         }
         return numFlights;
     }
-    return {calculateNumberOfFlights};
+
+    function checkAircraftRevision(distanceLimit, distancesArray) {
+        let totalDistance = 0;
+        let distance;
+
+        for (distance of distancesArray) {
+            totalDistance += distance;
+        }
+
+        if (totalDistance > distanceLimit) {
+            throw new Error("The total distance cannot be more than the distance limit");
+        }
+
+        // if (totalDistance > distanceLimit) {
+        //     throw new Error("Flight maximum allowed distance (" + distanceLimit + ") exceeded. No flight is allowed any longer, you need to make the revision immediately");
+        // }
+
+        if (totalDistance <= (distanceLimit / 2)) {
+            return "The revision needs to be done within the next 3 months";
+        } else if (totalDistance <= 3 * distanceLimit / 4) {
+            return "The revision needs to be done within the next 2 months";
+        } else {
+            return "The revision needs to be done within the next month";
+        }
+    }
+    return { calculateNumberOfFlights, checkAircraftRevision };
 }
 
 module.exports = Flights();
